43211b542c91b9f903805c9389bc57560486db2e
PythonSyntax1 <51872355+PythonSyntax1@users.noreply.github.com>
2020-09-06 11:25:49 -0700

    Update QuizTypeStrategy.java

--- app/src/main/java/model/QuizTypeStrategy.java
+++ app/src/main/java/model/QuizTypeStrategy.java
@@ -6,6 +6,6 @@ public interface QuizTypeStrategy {
     
     //The current application does not dynamically chose a strategy at runtime because each gamemode has a unique view,
     //but this allows for easy extension if a view is to be re-used in the future with different question types with the same 
-    //quiz. For example, MC quiz that asks algebra, or MC quiz with mixed questions.
+    //view. For example, MC quiz that asks algebra, or MC quiz with mixed questions.
     ArrayList<QuestionInterface> createQuiz(int quizLength);
 }
